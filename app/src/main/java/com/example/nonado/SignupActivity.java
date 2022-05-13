6c0cd55677dcdf3ceda44b8cb65ede794aacdc89
Sarah <ysarah0503@gmail.com>
2022-05-14 01:11:03 +0900

    Update

--- app/src/main/java/com/example/nonado/SignupActivity.java
+++ app/src/main/java/com/example/nonado/SignupActivity.java
@@ -1,27 +1,14 @@
 package com.example.nonado;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/nonado/SignupActivity.java
 import android.os.Bundle;
 
 public class SignupActivity extends AppCompatActivity {
-========
-public class detailActivity extends AppCompatActivity {
->>>>>>>> jjeongjae:app/src/main/java/com/example/nonado/detailActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<<< HEAD:app/src/main/java/com/example/nonado/SignupActivity.java
         setContentView(R.layout.activity_signup);
-========
-        setContentView(R.layout.activity_detail);
->>>>>>>> jjeongjae:app/src/main/java/com/example/nonado/detailActivity.java
     }
 }
--- app/src/main/java/com/example/nonado/detailActivity.java
+++ app/src/main/java/com/example/nonado/detailActivity.java
@@ -7,21 +7,11 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/nonado/SignupActivity.java
-import android.os.Bundle;
-
-public class SignupActivity extends AppCompatActivity {
-========
 public class detailActivity extends AppCompatActivity {
->>>>>>>> jjeongjae:app/src/main/java/com/example/nonado/detailActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<<< HEAD:app/src/main/java/com/example/nonado/SignupActivity.java
-        setContentView(R.layout.activity_signup);
-========
         setContentView(R.layout.activity_detail);
->>>>>>>> jjeongjae:app/src/main/java/com/example/nonado/detailActivity.java
     }
 }
