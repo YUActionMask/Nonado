c13b250e9acac479097a1244704edab7701b300e
Sarah <ysarah0503@gmail.com>
2022-05-23 00:50:13 +0900

    Update Chat
    파이어베이스 사용

--- app/src/main/java/com/example/nonado/chatAdapter.java
+++ app/src/main/java/com/example/nonado/chatAdapter.java
@@ -1,5 +1,7 @@
 package com.example.nonado;
 
+import android.os.Build;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,6 +9,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
@@ -17,22 +20,30 @@ class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder>{
     private List<Chat> chatList;
     private String name;
 
+//    public ChatAdapter(List<Chat> chatData, String name){
+//        //MainActivity.java에서 받은 데이터들을 저장
+//        chatList = chatData;
+//        this.name = name;
+//    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
         private TextView nameTv;
         private TextView msgTv;
         private View rootView;
+        public LinearLayout msgLinear;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
              nameTv = itemView.findViewById(R.id.nameTv);
              msgTv = itemView.findViewById(R.id.msgTv);
+            msgLinear = itemView.findViewById(R.id.msgLinear);
              rootView = itemView;
 
              itemView.setEnabled(true);
              itemView.setClickable(true);
+
         }
     }
     public ChatAdapter(List<Chat> chatData, String name){
@@ -49,22 +60,30 @@ class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder>{
         return myViewHolder;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void onBindViewHolder(@NonNull ChatAdapter.MyViewHolder holder, int position) {
         Chat chat = chatList.get(position);
 
         holder.nameTv.setText(chat.getName());
         holder.msgTv.setText(chat.getMsg());
+
+        if(chat.getName().equals(this.name)){
+            holder.nameTv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+            holder.msgTv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+
+            holder.msgLinear.setGravity(Gravity.RIGHT);
+        } else {
+            holder.nameTv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            holder.msgTv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+
+            holder.msgLinear.setGravity(Gravity.LEFT);
+        }
     }
 
     @Override
     public int getItemCount() {
-        if(chatList == null){
-            return 0;
-        }
-        else{
-            return chatList.size();
-        }
+        return chatList == null ? 0: chatList.size();
     }
 
     //추가 메소드
