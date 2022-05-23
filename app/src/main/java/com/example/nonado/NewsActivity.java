83f183d82854aeafda5da1ff8cd46c59d523ea67
21912045 <94848655+21912045@users.noreply.github.com>
2022-05-23 17:21:41 +0900

    /

--- app/src/main/java/com/example/nonado/NewsActivity.java
+++ app/src/main/java/com/example/nonado/NewsActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DetailActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_news);
     }
 }
