5f0740615c3b5c8a1515936e6f1b1b04934014e1
21912045 <94848655+21912045@users.noreply.github.com>
2022-05-27 01:09:36 +0900

    파이어베이스 연동

--- app/src/main/java/com/example/nonado/HomeActivity.java
+++ app/src/main/java/com/example/nonado/HomeActivity.java
@@ -1,14 +1,41 @@
 package com.example.nonado;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     Button plus, info, notice;
+    String title;
+    ListView listView;
+    private ChildEventListener mChild;
+    private ArrayAdapter<String> adapter;
+    List<Object> Array = new ArrayList<Object>();
+
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = database.getReference("Post");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +45,10 @@ public class HomeActivity extends AppCompatActivity {
         plus = (Button) findViewById(R.id.plus);
         notice = (Button) findViewById(R.id.notice);
         info = (Button) findViewById(R.id.info);
-
+        listView = (ListView) findViewById(R.id.listView);
+        //initDatabase;
+        adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        listView.setAdapter(adapter);
 //       notice.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -42,5 +72,65 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+
+                    String msg2 = messageData.getValue().toString();
+                    msg2 = msg2.substring(msg2.lastIndexOf("=")+1);
+                    Array.add(msg2);
+                    adapter.add(msg2);
+
+                }
+                adapter.notifyDataSetChanged();
+                listView.setSelection(adapter.getCount() - 1);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
+    private void initDatabase() {
+
+
+        mChild = new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        databaseReference.addChildEventListener(mChild);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        databaseReference.removeEventListener(mChild);
+    }
+
 }
