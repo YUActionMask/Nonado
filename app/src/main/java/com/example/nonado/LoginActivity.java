c9016d2ebd549fc199d63da4da0ab0f22308745c
sarah0503 <ysarah0503@gmail.com>
2022-09-07 15:50:17 +0900

    [update] 동네인증 alert
    오류남

--- app/src/main/java/com/example/nonado/LoginActivity.java
+++ app/src/main/java/com/example/nonado/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.nonado;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,6 +34,11 @@ public class LoginActivity extends AppCompatActivity {
     private Button buttonLogIn;
     private Button buttonSignUp;
     private Button buttonFindId;
+
+    private FirebaseUser user;
+    private DatabaseReference mDatabase;
+    private UserAccount userAccount;
+    private String location = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,10 @@ public class LoginActivity extends AppCompatActivity {
         id = findViewById(R.id.id);
         password = findViewById(R.id.password);
 
+        //데이터 베이스에서 사용자 받아오기
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String user_id = user.getEmail().split("@")[0];
+        mDatabase = FirebaseDatabase.getInstance().getReference("User").child(user_id);
 
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,10 +77,55 @@ public class LoginActivity extends AppCompatActivity {
                 firebaseAuth.signInWithEmailAndPassword(strId, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
                         if(task.isSuccessful()){
-                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                            startActivity(intent);
-                            finish();
+
+                            Log.d("milky",  "주소찾기");
+                            ValueEventListener postListener = new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    //for (DataSnapshot userData : dataSnapshot.getChildren()) {
+                                    location = dataSnapshot.child("location").getValue().toString();
+                                    //}
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    // Getting Post failed, log a message
+                                    Log.w("milky", "loadPost:onCancelled", databaseError.toException());
+                                }
+                            };
+                            mDatabase.addValueEventListener(postListener);
+
+                            Log.d("milky", location);
+
+                            if(location.equals("null")){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setTitle("동네인증이 필요합니다. ").setMessage("확인을 누르면 동네인증이 진행됩니다. ");
+
+                                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(LoginActivity.this, NeighborhoodCertificationActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
+
+                                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                });
+                                AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                            else{
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
                         }
                         else{
                             Toast.makeText(LoginActivity.this, "아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
@@ -75,13 +138,13 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-//        buttonFindId.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(LoginActivity.this, FindActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        buttonFindId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, FindActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
