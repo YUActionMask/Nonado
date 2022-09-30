8be5872ec34f0f73bdd763fa7d5a9e825cadbd33
21912045 <94848655+21912045@users.noreply.github.com>
2022-09-30 21:43:07 +0900

    참여 리스트 & 결제 실행 전단계

--- app/src/main/java/com/example/nonado/ChangeinfoActivity.java
+++ app/src/main/java/com/example/nonado/ChangeinfoActivity.java
@@ -1,5 +1,7 @@
 package com.example.nonado;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,14 +11,18 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -30,26 +36,39 @@ import java.io.InputStream;
 
 public class ChangeinfoActivity extends AppCompatActivity {
 
+    private TextView mTextViewshowid;
+
+
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private Button mBtnLogout, mBtnSignout, mBtnUploadImages;
+    private Button mBtnLogout, mBtnSignout, mBtnUploadImages, mBtnLocationChange,mBtnChange;
+    private EditText mEditpw;
 
     private final int GALLERY_CODE = 10;
     private ImageView mPhoto;
     private FirebaseStorage storage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changeinfo);
 
+        mTextViewshowid = findViewById(R.id.showid);
+        mEditpw = findViewById(R.id.changepw);
+
         mBtnLogout = findViewById(R.id.logout_btn);
         mBtnSignout = findViewById(R.id.signout_btn);
+        mBtnLocationChange = findViewById(R.id.local_amend_btn);
+        mBtnChange =findViewById(R.id.amend_btn);
         mBtnUploadImages = findViewById(R.id.imageUpload_btn);
         mPhoto = (ImageView)findViewById(R.id.img);
         storage = FirebaseStorage.getInstance();
 
+        mTextViewshowid.setText(firebaseAuth.getCurrentUser().getEmail());
+
+
+
+        //by재은, 로그아웃 버튼 구현
         mBtnLogout.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -64,6 +83,7 @@ public class ChangeinfoActivity extends AppCompatActivity {
         } );
 
 
+        //by재은, 회원탈퇴 버튼 구현
         mBtnSignout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,6 +108,32 @@ public class ChangeinfoActivity extends AppCompatActivity {
                         loadAlbum();
                         break;
                 }
+            }
+        });
+        //by재은, 위치수정 버튼 구현
+        mBtnLocationChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                location_Change_showDialog();
+            }
+        });
+
+        mBtnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().getCurrentUser().updatePassword(mEditpw.toString()).addOnCompleteListener(ChangeinfoActivity.this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ChangeinfoActivity.this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else{
+                            Toast.makeText(ChangeinfoActivity.this, "비밀번호 변경에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
 
@@ -127,6 +173,26 @@ public class ChangeinfoActivity extends AppCompatActivity {
             });
 
         }
+    }
+    private void location_Change_showDialog(){
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(ChangeinfoActivity.this)
+                .setTitle("위치 변경")
+                .setMessage("위치를 변경하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(ChangeinfoActivity.this, NeighborhoodCertificationActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
     }
 
 
