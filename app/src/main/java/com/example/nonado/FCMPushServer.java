cb707129893f0cbac79a1e3c3e950a3dbbd7dfa4
Sarah <ysarah0503@gmail.com>
2022-11-14 01:30:27 +0900

    [UPDATE] 상세 수정

--- app/src/main/java/com/example/nonado/FCMPushServer.java
+++ app/src/main/java/com/example/nonado/FCMPushServer.java
@@ -1,6 +1,7 @@
 package com.example.nonado;
 
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -48,7 +49,6 @@ public class FCMPushServer extends FirebaseMessagingService {
             public void onComplete(@NonNull Task<String> task) {
                 if(task.isSuccessful()){
 
-                    Log.d("milkyLog", task.getResult());
                     user = FirebaseAuth.getInstance().getCurrentUser();
                     user_id = user.getEmail().split("@")[0];
                     userDatabase.child("User").child(user_id).child("token").setValue(task.getResult());
@@ -58,16 +58,13 @@ public class FCMPushServer extends FirebaseMessagingService {
                                 @Override
                                 public void onComplete(@NonNull Task<String> task) {
                                     if (!task.isSuccessful()) {
-                                        Log.w("milkyLog", "Fetching FCM registration token failed", task.getException());
+                                        //Log.w("milkyLog", "Fetching FCM registration token failed", task.getException());
                                         return;
                                     }
 
                                     // Get new FCM registration token
                                     String token = task.getResult();
 
-                                    // Log and toast
-                                    //String msg = getString(R.string.msg_token_fmt, token);
-                                    Log.d("milkyLog", "ss = " + token);
                                 }
                             });
 
@@ -87,20 +84,23 @@ public class FCMPushServer extends FirebaseMessagingService {
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         //수신한 메시지를 처리
         super.onMessageReceived(remoteMessage);
+        Log.d("milkyLog", "Start");
         if(remoteMessage.getData().size()>0){
             showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         }
+
     }
 
 
     public void showNotification(String title, String message){
 
+
         //팝업 터치 시 HomeActivity로 이동
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(getApplicationContext(), MyinfoActivity.class);
 
         String channel_id = getString(R.string.default_notification_channel_id);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
 
         // 기본 사운드 알람음 설정.
         Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -114,7 +114,6 @@ public class FCMPushServer extends FirebaseMessagingService {
                 .setVibrate(new long[]{1000,1000,1000})
                 .setOnlyAlertOnce(true)
                 .setContentIntent(pendingIntent);
-
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
