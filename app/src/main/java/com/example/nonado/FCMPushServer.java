37ef646f012fc22b768b1477ea189227d0aa1600
Sarah <ysarah0503@gmail.com>
2022-11-15 04:34:52 +0900

    [UPDATE] Login
    자동 로그인

--- app/src/main/java/com/example/nonado/FCMPushServer.java
+++ app/src/main/java/com/example/nonado/FCMPushServer.java
@@ -36,56 +36,53 @@ public class FCMPushServer extends FirebaseMessagingService {
     private DatabaseReference postDatabase;
     private DatabaseReference userDatabase;
     private
-    String comment, post_name, location;
-    private
     Intent intent = null;
     private PendingIntent pendingIntent = null;
 
-    //private  static final String TAG = "FCMPushServer";
+    private  static final String TAG = "FCMPushServer";
 
-//    private FirebaseAuth firebaseAuth;
-//    private DatabaseReference userDatabase;
-//    private FirebaseUser user;
-//    private String user_id;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private String user_id;
 
 
-//    public FCMPushServer() {
-//        super();
-//        Task<String> token = FirebaseMessaging.getInstance().getToken();
-//
-//        firebaseAuth = FirebaseAuth.getInstance();
-//        userDatabase = FirebaseDatabase.getInstance().getReference();
-//
-//
-//        token.addOnCompleteListener(new OnCompleteListener<String>() {
-//            @Override
-//            public void onComplete(@NonNull Task<String> task) {
-//                if(task.isSuccessful()){
-//
-//
-//                    user = FirebaseAuth.getInstance().getCurrentUser();
-//                    user_id = user.getEmail().split("@")[0];
-//                    userDatabase.child("User").child(user_id).child("token").setValue(task.getResult());
-//
-//                    FirebaseMessaging.getInstance().getToken()
-//                            .addOnCompleteListener(new OnCompleteListener<String>() {
-//                                @Override
-//                                public void onComplete(@NonNull Task<String> task) {
-//                                    if (!task.isSuccessful()) {
-//                                        //Log.w("milkyLog", "Fetching FCM registration token failed", task.getException());
-//                                        return;
-//                                    }
-//
-//                                    // Get new FCM registration token
-//                                    String token = task.getResult();
-//
-//                                }
-//                            });
-//
-//                }
-//            }
-//        });
-//    }
+    public FCMPushServer() {
+        super();
+        Task<String> token = FirebaseMessaging.getInstance().getToken();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        userDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        token.addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(task.isSuccessful()){
+
+
+                    user = FirebaseAuth.getInstance().getCurrentUser();
+                    user_id = user.getEmail().split("@")[0];
+                    userDatabase.child("User").child(user_id).child("token").setValue(task.getResult());
+
+                    FirebaseMessaging.getInstance().getToken()
+                            .addOnCompleteListener(new OnCompleteListener<String>() {
+                                @Override
+                                public void onComplete(@NonNull Task<String> task) {
+                                    if (!task.isSuccessful()) {
+                                        //Log.w("milkyLog", "Fetching FCM registration token failed", task.getException());
+                                        return;
+                                    }
+
+                                    // Get new FCM registration token
+                                    String token = task.getResult();
+
+                                }
+                            });
+
+                }
+            }
+        });
+    }
 
 
     @Override
@@ -109,6 +106,10 @@ public class FCMPushServer extends FirebaseMessagingService {
 
     public void showNotification(String title, String message, String intent_str){
 
+        final String[] comment = new String[1];
+        final String[] post_name = new String[1];
+        final String[] location = new String[1];
+
         if(intent_str.equals("DetailActivity")){
 
             intent = new Intent(this, DetailActivity.class);
@@ -117,24 +118,49 @@ public class FCMPushServer extends FirebaseMessagingService {
             ValueEventListener postListener = new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    comment = dataSnapshot.child("comment").getValue().toString();
-                    post_name = dataSnapshot.child("name").getValue().toString();
+                    comment[0] = dataSnapshot.child("comment").getValue().toString();
+                    post_name[0] = dataSnapshot.child("name").getValue().toString();
 
-                    userDatabase = FirebaseDatabase.getInstance().getReference("User").child(post_name);
+                    userDatabase = FirebaseDatabase.getInstance().getReference("User").child(post_name[0]);
 
                     ValueEventListener userListener = new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            location = dataSnapshot.child("location").getValue().toString();
+                            location[0] = dataSnapshot.child("location").getValue().toString();
 
                             intent.putExtra("title", title);
-                            intent.putExtra("comment", comment);
-                            intent.putExtra("writer", post_name);
-                            Log.d("milkyLog", "dd " + title);
-                            intent.putExtra("name",post_name);
-                            intent.putExtra("location",location);
+                            intent.putExtra("comment", comment[0]);
+                            intent.putExtra("writer", post_name[0]);
+                            intent.putExtra("name", post_name[0]);
+                            intent.putExtra("location",location[0]);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,PendingIntent.FLAG_IMMUTABLE);
+                            String channel_id = getString(R.string.default_notification_channel_id);
 
+
+
+                            // 기본 사운드 알람음 설정.y
+                            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                            NotificationCompat.Builder notificationBuilder =
+                                    new NotificationCompat.Builder(getApplicationContext(), channel_id)
+                                            .setSmallIcon(R.drawable.ic_bear)
+                                            .setContentTitle(title)
+                                            .setContentText(message)
+                                            .setSound(uri)
+                                            .setVibrate(new long[]{1000,1000,1000})
+                                            .setContentIntent(pendingIntent);
+
+                            NotificationManager notificationManager =
+                                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                NotificationChannel notificationChannel = new NotificationChannel(channel_id, "CHN_NAME", NotificationManager.IMPORTANCE_HIGH);
+                                notificationChannel.setSound(uri, null);
+                                notificationManager.createNotificationChannel(notificationChannel);
+                            }
+
+                            notificationManager.notify(0 ,notificationBuilder.build());
                         }
 
                         @Override
@@ -142,6 +168,7 @@ public class FCMPushServer extends FirebaseMessagingService {
                         }
                     };
                     userDatabase.addValueEventListener(userListener);
+
 
                 }
 
@@ -155,32 +182,7 @@ public class FCMPushServer extends FirebaseMessagingService {
         }
 
 
-        String channel_id = getString(R.string.default_notification_channel_id);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-
-        // 기본 사운드 알람음 설정.
-        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(getApplicationContext(), channel_id)
-                .setSmallIcon(R.drawable.ic_bear)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setSound(uri)
-                .setVibrate(new long[]{1000,1000,1000})
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(channel_id, "CHN_NAME", NotificationManager.IMPORTANCE_HIGH);
-            notificationChannel.setSound(uri, null);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-        notificationManager.notify(0 ,notificationBuilder.build());
 
     }
 
